0f6cb258d1df377ec8e8086fe3ab407a5375ef95
Ronnel Abrigo <ronnelabrigo@gmail.com>
2022-01-10 18:56:02 -0800

    added materialize navbar and card. test API to pull image, name, and description

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,25 @@
+var petImage = document.querySelector('#pet-image');
+var petName = document.querySelector('.card-title');
+var petDescription = document.querySelector('.card-content');
 // The URL below is set to search for dogs
-var apiURL = "https://api.rescuegroups.org/v5/public/animals/search/available/dogs/";
-fetch(apiURL, {
+var apiUrl = "https://api.rescuegroups.org/v5/public/animals/search/available/dogs/";
+
+fetch(apiUrl, {
         headers: {
                 'Content-Type': 'application/vnd.api+json',
-                // our API key
                 'Authorization': 'D5eT1vpr',
+                // Above: our API key
         }
 })
         .then(function (response) {
                 return response.json();
         })
         .then(function (data) {
+                petImage.setAttribute("src", data.data[0].attributes.pictureThumbnailUrl);
+                petName.textContent = data.data[0].attributes.name;
+                petDescription.innerHTML = data.data[0].attributes.descriptionHtml;
+                // For debugging:
                 console.log(data);
+                console.log(data.data[0].attributes.pictureThumbnailUrl);
               });
             
