f771b664769b5df930bde70f28a06e3eb1e2d8c1
Raw Ness <rawness@Raws-Mac-mini.local>
2022-01-12 11:17:35 -0800

    updated first page

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,8 @@
-var petImage = document.querySelector('#pet-image');
-var petName = document.querySelector('.card-title');
-var petDescription = document.querySelector('.card-content');
-// The URL below is set to search for dogs
-var apiUrl = "https://api.rescuegroups.org/v5/public/animals/search/available/dogs/";
+// the styling stays the same even without this code
+// but material documentation says to include it
 
-fetch(apiUrl, {
-        headers: {
-                'Content-Type': 'application/vnd.api+json',
-                'Authorization': 'D5eT1vpr',
-                // Above: our API key
-        }
-})
-        .then(function (response) {
-                return response.json();
-        })
-        .then(function (data) {
-                petImage.setAttribute("src", data.data[0].attributes.pictureThumbnailUrl);
-                petName.textContent = data.data[0].attributes.name;
-                petDescription.innerHTML = data.data[0].attributes.descriptionHtml;
-                // For debugging:
-                console.log(data);
-                console.log(data.data[0].attributes.pictureThumbnailUrl);
-              });
-            
+M.AutoInit();
+$(document).ready(function() {
+    $('select').formSelect();
+});
+var instance = M.FormSelect.getInstance(elem);
