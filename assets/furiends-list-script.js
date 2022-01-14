5e64f1049b68d28b990d58a0ade8fc6c51f3ab15
Reid Somers Flamm <reidsomersflamm@gmail.com>
2022-01-14 10:57:56 -0800

    Made changes to furiends-list js output, and general layout/css styling

--- assets/furiends-list-script.js
+++ assets/furiends-list-script.js
@@ -68,49 +68,68 @@ var resultContainer = document.getElementById("results")
 //TODO: Once the local storage is set, enable this to populate the page
 function populatePage()
 {
+  for (var i = 0; i < 5; i++)
+  {
   // for (var i = 0; i < savedAnimalData.length; i++)
   // {
+       var resultPetList = document.createElement('ul');
        var resultPetImage = document.createElement('img');
        var resultPetName = document.createElement('p');
        var resultPetType = document.createElement('p');
        var resultPetAge = document.createElement('p');
        var resultPetBreed = document.createElement('p');
        var resultPetCoatLength = document.createElement('p');
-       var resultPetLocation = document.createElement('p');
-       var resultPetAdoptionLink = document.createElement('p');
+       var newCard = document.createElement("div");
+       var adoptButton = document.createElement('btn');
+
+      resultPetName.className += " class-content pet-card-body-text";
+      resultPetType.className += " class-content pet-card-body-text";
+      resultPetAge.className += " class-content pet-card-body-text";
+      resultPetBreed.className += " class-content pet-card-body-text";
+      resultPetCoatLength.className += " class-content pet-card-body-text";
 
  // TODO: Need to find other attributes to populate the remaining sections
  // Adding the text content from the collected results so they can be later appended to the results html page
-       resultPetImage.textContent = "Image Goes Here";
        resultPetName.textContent = "Name: ";
        resultPetType.textContent = "Type: ";
        resultPetBreed.textContent = "Breed: ";
        resultPetAge.textContent = "Age: ";
        resultPetCoatLength.textContent = "Coat Length: ";
-       resultPetLocation.textContent = "Location: ";
-       resultPetAdoptionLink.textContent = "Adoption Link: ";
+       adoptButton.textContent = "Adopt Me";
+
+
+       resultPetImage.src = "https://i.imgur.com/MU2dD8E.jpeg";
+       resultPetImage.width = "300";
+       resultPetImage.className = "pet-image z-depth-2"
 
 
  // Append section that will attach the above text content and apply it to the resultContainer on the results html page
-       var newCard = document.createElement("div");
        newCard.className="card furiends-cards";
        newCard.id = "card";
-       console.log(newCard.id);
-       resultContainer.append(newCard);
+       //add + i to newcard.id when for loop happens
 
+       adoptButton.className="btn adopt-button waves-effect waves-light";
+       console.log(newCard.id);
+
+
+       resultContainer.append(newCard);
        newCard.append(resultPetImage);
-       newCard.append(resultPetName);
-       newCard.append(resultPetType);
-       newCard.append(resultPetBreed);
-       newCard.append(resultPetAge);
-       newCard.append(resultPetCoatLength);
-       newCard.append(resultPetLocation);
-       newCard.append(resultPetAdoptionLink);
-  // }
+       newCard.append(resultPetList);
+       resultPetList.append(resultPetName);
+       resultPetList.append(resultPetType);
+       resultPetList.append(resultPetBreed);
+       resultPetList.append(resultPetAge);
+       resultPetList.append(resultPetCoatLength);
+       resultPetList.append(adoptButton);
+
+
+  }
 }
 
 function main(){
-  var savedAnimalData = JSON.parse(localStorage.getItem('pettentials'))
+  var savedAnimalData = JSON.parse(localStorage.getItem('pettentials'));
+
+  console.log(savedAnimalData);
   populatePage();
 
 }
