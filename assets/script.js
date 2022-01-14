2e04fe275786e81f8e29f45732043076182cdeeb
Raw Ness <rawness@Raws-Mac-mini.local>
2022-01-13 16:15:49 -0800

    changes url based on parameters

--- assets/script.js
+++ assets/script.js
@@ -12,14 +12,16 @@ fetch('https://dog-api.matthewswar.com/api/facts')
         petfacts1.innerText = data.facts 
         console.log(data)});
 
- fetch('https://cat-fact.herokuapp.com/facts')
+fetch('https://cat-fact.herokuapp.com/facts')
     .then(response => response.json())
     .then(data => {
         var petfacts2 = document.querySelector('.cat-facts');
         // petfacts2.innerText = data.text; 
         console.log(data)});
     
-
+var catsOrDogs;
+var needPictures;        
+var apiUrl;
 var typeParameter;
 var genderParameter;
 var sizeParameter;
@@ -29,23 +31,59 @@ var picturesParameter;
 var shotsParameter;
 var petFriendlyParameter;
 
+function fetchData () {
+  fetch(apiUrl, {
+    headers: {
+            'Content-Type': 'application/vnd.api+json',
+            'Authorization': 'D5eT1vpr',
+            // Above: our API key
+      }})
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (data) {
+          console.log(data);
+      })
+}
+
 function fetchFunction() {
-typeParameter = document.getElementById('type-parameter').value;
-genderParameter = document.getElementById('gender-parameter').value;
-sizeParameter = document.getElementById('size-parameter').value;
-ageParameter = document.getElementById('age-parameter').value;
-coatParameter = document.getElementById('coat-parameter').value;
-picturesParameter = document.getElementById('pictures-parameter').checked;
-shotsParameter = document.getElementById('shots-parameter').checked;
-petFriendlyParameter = document.getElementById('pet-friendly-parameter').checked;
-console.log(typeParameter);
-console.log(genderParameter);
-console.log(sizeParameter);
-console.log(ageParameter);
-console.log(coatParameter);
-console.log(picturesParameter);
-console.log(shotsParameter);
-console.log(petFriendlyParameter);
+  typeParameter = document.getElementById('type-parameter').value;
+  genderParameter = document.getElementById('gender-parameter').value;
+  sizeParameter = document.getElementById('size-parameter').value;
+  ageParameter = document.getElementById('age-parameter').value;
+  coatParameter = document.getElementById('coat-parameter').value;
+  picturesParameter = document.getElementById('pictures-parameter').checked;
+  shotsParameter = document.getElementById('shots-parameter').checked;
+  petFriendlyParameter = document.getElementById('pet-friendly-parameter').checked;
+  // For debugging: 
+  // console.log(typeParameter);
+  // console.log(genderParameter);
+  // console.log(sizeParameter);
+  // console.log(ageParameter);
+  // console.log(coatParameter);
+  // console.log(picturesParameter);
+  // console.log(shotsParameter);
+  // console.log(petFriendlyParameter);
+  if (typeParameter == 1) {
+    catsOrDogs = "cats";
+    console.log(catsOrDogs);
+  } else if (typeParameter == 2) {
+    catsOrDogs = "dogs";
+    console.log(catsOrDogs);
+  } else {
+    catsOrDogs = "";
+    console.log("no pet type chosen")
+  }
+  if (picturesParameter) {
+    needPictures = "/&include=pictures";
+    console.log(picturesParameter);
+  } else if (!picturesParameter) {
+    needPictures = "";
+    console.log(picturesParameter);
+  }
+  apiUrl = "https://api.rescuegroups.org/v5/public/animals/search/available/" + catsOrDogs + needPictures;
+  console.log(apiUrl);
+  fetchData();
 }
 
 document.querySelector('#furiends-button').addEventListener('click',function() {
