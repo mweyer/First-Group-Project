11e9d20654506f837fcc059df272fca2c50e3ff9
Reid Somers Flamm <reidsomersflamm@gmail.com>
2022-01-17 22:21:50 -0800

    css style changes and adoption button output

--- assets/furiends-list-script.js
+++ assets/furiends-list-script.js
@@ -1,92 +1,37 @@
 var resultContainer = document.getElementById("results")
+var adoptionURL = "";
 
-// var apiUrl = "https://api.rescuegroups.org/v5/public/animals/";
-
-
-
-// function getAnimalData(listOfIds){
-//   listOfIds.forEach(element => {
-//     fetch(apiUrl+element, { 
-//       headers: {
-//       'Content-Type': 'application/vnd.api+json',
-//       'Authorization': 'D5eT1vpr',
-//       // Above: our API key
-//     }
-//   })
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then( function(responseBody){
-//       console.log(responseBody)
-
-//       //TODO: For loop based upon stored results
-//         //For loop to append result data to the results page
-//          for (var i = 0; i < 10; i++)
-//          {
-//               var resultPetImage = document.createElement('img');
-//               var resultPetName = document.createElement('p');
-//               var resultPetType = document.createElement('p');
-//               var resultPetAge = document.createElement('p');
-//               var resultPetBreed = document.createElement('p');
-//               var resultPetCoatLength = document.createElement('p');
-//               var resultPetLocation = document.createElement('p');
-//               var resultPetAdoptionLink = document.createElement('p');
-
-//         // TODO: Need to find other attributes to populate the remaining sections
-//         // Adding the text content from the collected results so they can be later appended to the results html page
-//               resultPetImage.textContent = "Image Goes Here";
-//               resultPetName.textContent = "Name: ";
-//               resultPetType.textContent = "Type: ";
-//               resultPetBreed.textContent = "Breed: ";
-//               resultPetAge.textContent = "Age: ";
-//               resultPetCoatLength.textContent = "Coat Length: ";
-//               resultPetLocation.textContent = "Location: ";
-//               resultPetAdoptionLink.textContent = "Adoption Link: ";
-
-
-//         // Append section that will attach the above text content and apply it to the resultContainer on the results html page
-//               var newCard = document.createElement("div");
-//               newCard.className="card furiends-cards";
-//               newCard.id = "card" + i;
-//               console.log(newCard.id);
-//               resultContainer.append(newCard);
-
-//               newCard.append(resultPetImage);
-//               newCard.append(resultPetName);
-//               newCard.append(resultPetType);
-//               newCard.append(resultPetBreed);
-//               newCard.append(resultPetAge);
-//               newCard.append(resultPetCoatLength);
-//               newCard.append(resultPetLocation);
-//               newCard.append(resultPetAdoptionLink);
-//          }
-      
-//     })
-
-//   });
-// }
 //TODO: Once the local storage is set, enable this to populate the page
 function populatePage()
 {
   var savedAnimalData = JSON.parse(localStorage.getItem('pettentials'));
+ 
   console.log(savedAnimalData)
   // for (var i = 0; i < 50; i++)
   // {
   for (var i = 0; i < savedAnimalData.length; i++)
   {
+     var org = JSON.parse(localStorage.getItem("orgs"));
+
        var newRow = document.createElement('row');
        var newCol = document.createElement('col');
        var newCard = document.createElement("div");
        var resultPetList = document.createElement('ul');
        var resultPetImage = document.createElement('img');
        var resultPetName = document.createElement('p');
-       var resultPetNameTitle = document.createElement('h2');
+       var resultPetNameTitle = document.createElement('p');
        var resultPetAge = document.createElement('p');
        var resultPetBreed = document.createElement('p');
        var resultPetCoatLength = document.createElement('p');
        var buttonCard = document.createElement('div');
        var adoptButton = document.createElement('btn');
        var removeButton = document.createElement('btn');
+
+     // var urlFilter = .filter(function(url){
+     //      return url;
+     // });
+     // console.log(urlFilter);
+
 
       resultPetName.className += " class-content pet-card-body-text";
       resultPetNameTitle.className += " class-content pet-card-title-text";
@@ -95,23 +40,30 @@ function populatePage()
       resultPetCoatLength.className += " class-content pet-card-body-text";
 
 
+     console.log(org);
+
+     //  var orgID = savedAnimalData[i].relationships.orgs.data[0].id;
+     //  console.log(orgID);
+
+     //  if(orgID === org.attributes.type)
+     //  {
+
+     //  }
 
  // TODO: Need to find other attributes to populate the remaining sections
  // Adding the text content from the collected results so they can be later appended to the results html page
-      resultPetNameTitle = "Name: ";
-      resultPetName.textContent = resultPetNameTitle + savedAnimalData[i].attributes.name;
-       resultPetBreed.textContent = "Breed: " + savedAnimalData[i].attributes.breedPrimary;
-       resultPetAge.textContent = "Age: " + savedAnimalData[i].attributes.ageGroup;
-       resultPetCoatLength.textContent = "Coat Length: " + savedAnimalData[i].attributes.coatLength;
-       adoptButton.textContent = "Adopt Me";
-       removeButton.textContent = "Remove";
+      resultPetName.innerHTML = "<b>Name: </b>" + savedAnimalData[i].attributes.name;
+       resultPetBreed.innerHTML = "<b>Breed: </b>" + savedAnimalData[i].attributes.breedPrimary;
+       resultPetAge.innerHTML = "<b>Age: </b>" + savedAnimalData[i].attributes.ageGroup;
+       resultPetCoatLength.innerHTML = "<b>Coat Length: </b>" + savedAnimalData[i].attributes.coatLength;
+       adoptButton.innerHTML = "Adopt Me";
+     //   removeButton.innerHTML = "Remove";
 
-
-       resultPetImage.src = savedAnimalData[i].attributes.pictureThumbnailUrl;
+     //Pulling the image and resizing them for each pet
+       resultPetImage.src = savedAnimalData[i].pictureURL;
        resultPetImage.width = "300";
        resultPetImage.height = "300";
        resultPetImage.className = "pet-image z-depth-2"
-
 
  // Append section that will attach the above text content and apply it to the resultContainer on the results html page
        newCard.className="card furiends-cards";
@@ -120,10 +72,8 @@ function populatePage()
        buttonCard.className="card button-card"
        //add + i to newcard.id when for loop happens
 
-       adoptButton.className="btn adopt-button waves-effect waves-light";
-       removeButton.className="btn remove-button waves-effect red waves-red";
-       console.log(newCard.id);
-
+       adoptButton.className="btn modal-trigger adopt-button waves-effect waves-light";
+     //  removeButton.className="btn remove-button waves-effect red waves-red";
 
        resultContainer.append(newCard);
        newCard.append(resultPetImage);
@@ -135,6 +85,15 @@ function populatePage()
        resultPetList.append(buttonCard);
        buttonCard.append(adoptButton);
      //   buttonCard.append(removeButton);
+
+     //TODO: Add modal for adoption popup
+     adoptButton.addEventListener("click", adoptLink);
+
+     function adoptLink()
+     {
+          window.open(org[0].attributes.url, "_blank");
+
+     }
 
   }
 }
